d3a641c29e191eb20739b79a0a7786062c9aff1b
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-24 00:24:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+    static boolean isPrime(int num, int i) {
+        if ((num == i)) {
+            System.out.println("asal sayıdır ");
+            return true;
+
+        } else if (num % i == 0) {
+            System.out.println("asal sayı değildir");
+            return false;
+
+        }
+        return isPrime(num, i +1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz: ");
+        int num = input.nextInt();
+
+        isPrime(num,2) ;
     }
 }
